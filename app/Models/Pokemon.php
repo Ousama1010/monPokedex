34d6d79e32ae2d91f9f6ef13cd5ee1c42a98a853
ousama <ousama850@gmail.com>
2024-06-18 00:32:39 +0200

    le model

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -5,7 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pokemons extends Model
+class Pokemon extends Model
 {
     use HasFactory;
+
+    protected $table = 'pokemon';
+
+    protected $fillable = [
+        'name',
+        'hp',
+        'weight',
+        'height',
+        'img_path',
+        'primary_type_id',
+        'secondary_type_id'
+    ];
+
+    public function types()
+    {
+        return $this->belongsToMany(Type::class, 'pokemon_type', 'pokemon_id', 'type_id');
+    }
+
+    public function primaryType()
+    {
+        return $this->belongsTo(Type::class, 'primary_type_id');
+    }
+
+    public function secondaryType()
+    {
+        return $this->belongsTo(Type::class, 'secondary_type_id');
+    }
+
 }

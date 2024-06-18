c6cb3232236b86049778b1c59ffcc7b7f106f60a
ousama <ousama850@gmail.com>
2024-06-18 02:10:43 +0200

    controller pokemon request

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\AttackController;
+use App\Http\Controllers\PokemonController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::resource('types', TypeController::class);
+    Route::resource('attacks', AttackController::class);
+    Route::resource('pokemons', PokemonController::class);
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
